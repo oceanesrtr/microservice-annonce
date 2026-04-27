4c514d112fbc70c4e8246210a72c2c5d054f46be
Océane Soamiadanantenaina Rakotoarisoa <oceane.rakotoarisoa@uqtr.ca>
2026-04-27 03:02:43 -0400

    INITIAL COMMIT

--- src/main/java/ca/uqtr/projet/annonces/config/SecurityConfig.java
+++ src/main/java/ca/uqtr/projet/annonces/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ca.uqtr.projet.annonces.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,10 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> {})            // utilise le CorsConfig ci-dessus
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
